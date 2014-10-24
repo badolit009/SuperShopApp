00e92586a34b0d767e7beb553ed246dc4a2ea260
badolit009 <badolit009@gmail.com>
2014-10-24 23:54:57 +0600

    Database Upload Successfully

--- SuperShopApp/SuperShopApp/BLL/ProductBLL.cs
+++ SuperShopApp/SuperShopApp/BLL/ProductBLL.cs
@@ -12,46 +12,40 @@ namespace SuperShopApp.BLL
     class ProductBLL
     {
         private ProductGateway aProductGateway = new ProductGateway();
-
         public string Save(Product aProduct)
         {
             String msg = "";
+                if (aProduct.Code.Length > 3 || aProduct.Code.Length < 3)
+                {
+                    msg += "Plz Product Code 3 Character \n";
+                }
+                if (aProduct.Name.Length < 5 || aProduct.Name.Length > 10)
+                {
+                    msg += "Plz Product Name 5-10 character\n";
+                }
+
             if (HasThisCode(aProduct.Code) || HasThisName(aProduct.Name))
-            {
-
-                if (HasThisCode(aProduct.Code))
                 {
-                    msg += "Code Is Already Exits In System\n";
+                    if (HasThisCode(aProduct.Code))
+                    {
+                        msg += "Code Is Already Exits In System\n";
+                    }
+                    if (HasThisName(aProduct.Name))
+                    {
+                        msg += "\nName Is Already Exits In System";
+                    }
                 }
-
-                if (HasThisName(aProduct.Name))
-                {
-                    msg += "\nName Is Already Exits In System";
-                }
-            }
+            
             else
             {
-                if (aProduct.Code.Length>3||aProduct.Code.Length<3)
-                {
-                    return "Plz Type 3 Character";
-                }
-                if (aProduct.Name.Length<5||aProduct.Name.Length>10)
-                {
-                    return "Plz type 5-10 character";
-                }
-                    return aProductGateway.Save(aProduct);
-                
+                return aProductGateway.Save(aProduct);
             }
-
             return msg;
-
-        }
-
+            }
         private bool HasThisName(string name)
         {
             return aProductGateway.HasThisName(name);
         }
-
         private bool HasThisCode(string code)
         {
             return aProductGateway.HasThisCode(code);
